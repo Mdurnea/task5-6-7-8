51969cf394ee1ae5f783c2a2c9187f0c6e7ddded
Mdurnea <mihail.durnea@endava.com>
2020-10-28 14:45:17 +0200

    First Push Branch2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("here is some changes in Main branch");
+        System.out.println("making some changes in this Branch2");
     }
 }
