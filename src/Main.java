bea6a822ca15f7b05ad42cc27bbf2789f4e7a8a7
Mdurnea <mihail.durnea@endava.com>
2020-10-28 15:13:54 +0200

    Branch1 with methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 
 public class Main {
+    public static void methodRankPoints(double points) {
+        if (points >= 202.5) {
+            System.out.println("Rank:A1");
+        }else if (points >= 122.4) {
+            System.out.println("Rank:A2");
+        }else {
+            System.out.println("Rank:A3");
+        }
+    }
+    public static int minFunction(int n1, int n2) {
+        int min;
+        if (n1 > n2)
+            min = n2;
+        else
+            min = n1;
+
+        return min;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("here is some changes in Main branch");
+        System.out.println("Lowest number is: "+minFunction(87,65));
+
     }
 }
